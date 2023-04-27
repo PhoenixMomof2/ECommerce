37d4b72e015f81410fad356c9200ee1ffd3e798e
Lionel Mason <capitalcookhouse@gmail.com>
2023-04-27 11:28:30 -0400

    creates associations in models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+    has_many :carts
 end
+
+
+# create an array of items purchased. .unique call. may also need to use .flatten
+# create a model called item. grab carts, map throguh, find items to each individual cart 
