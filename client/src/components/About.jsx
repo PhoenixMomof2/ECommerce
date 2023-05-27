5f955c4fbdcf89298e1433b0d1cb3250490c6ce6
Lionel Mason <capitalcookhouse@gmail.com>
2023-05-26 23:38:36 -0400

    Edits the About route

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 // import "./../styles/About.css";
 // import {LinkX} from "react-scroll";
+import About2 from "./images/About2.jpg"
 
 const About = () => {
 
@@ -19,7 +20,16 @@ const About = () => {
         //             <button className="shop-button">SHOP NOW</button>
         //     </LinkX>
         // </div>
-        <div>About</div>
+        <div className="containter-fluid">
+        <div className="has-bg-img">
+         {/* <h2>Hero Section</h2> */}
+          {/* <h4>It's easy to set background image with Torus Kit</h4> */}
+          <img style={{height: "100vh"}} className="bg-img mx-auto img-fluid" src={About2}alt={About2}/>
+          <h4>About</h4>
+          <h6>Demeure is from The Bronx, NY. With a strong focus on simple yet powerful design, we are driven to create timeless work by portraying an aesthetic that is uniquely our own.</h6>
+        </div>
+  
+      </div>
     );
 
 };
--- client/src/components/auth/Home.jsx
+++ client/src/components/auth/Home.jsx
@@ -4,11 +4,11 @@ import subway from '../images/subway.jpg'
 const Home = () => {
   return (
     <div className="containter-fluid">
-    <div className="has-bg-img">
-    <h2>Hero Section</h2>
-    <h4>It's easy to set background image with Torus Kit</h4>
-    <img style={{height: "100vh"}} className="bg-img mx-auto img-fluid" src={subway}alt={subway}/>
-  </div>
+      <div className="has-bg-img">
+       <h2>Hero Section</h2>
+        <h4>It's easy to set background image with Torus Kit</h4>
+        <img style={{height: "100vh"}} className="bg-img mx-auto img-fluid" src={subway}alt={subway}/>
+      </div>
 
     </div>
   )
