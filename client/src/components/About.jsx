39bfe8d7b0b2262badc92a0cab9e2fa66c4b302b
Lionel Mason <capitalcookhouse@gmail.com>
2023-05-27 14:57:58 -0400

    Styles About component

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -1,30 +1,4 @@
-// import React from "react";
-// // import "./../styles/About.css";
-// // import {LinkX} from "react-scroll";
-// import About2 from "./images/About2.jpg"
-
-// const About = () => {
-
-//     return (
-   
-//         <div className="containter-fluid">
-//         <div className="has-bg-img">
-//          {/* <h2>Hero Section</h2> */}
-//           {/* <h4>It's easy to set background image with Torus Kit</h4> */}
-//           <img style={{height: "100vh"}} className="bg-img mx-auto img-fluid" src={About2}alt={About2}/>
-//           <h4>About</h4>
-//           <h6>Demeure is from The Bronx, NY. With a strong focus on simple yet powerful design, we are driven to create timeless work by portraying an aesthetic that is uniquely our own.</h6>
-//         </div>
-  
-//       </div>
-//     );
-
-// };
-
-// export default About;
-
 import React from "react";
-// import "./../styles/About.css";
 // import {LinkX} from "react-scroll";
 import About2 from "./images/About2.jpg";
 
